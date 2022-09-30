92116f875e1871f303db3f9c1e59c682afd5c555
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-30 10:40:44 -0500

    basic dom

---
+++ javascript_platzi/manipulacion_dom/script.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btncalcular');
+const presult = document.querySelector('#result');
+
+function btnonclick(){
+    console.log('escuchando eventos');
+    console.log(Number(input1.value) + Number(input2.value));
+    const suma = Number(input1.value) + Number(input2.value);
+    presult.innerText = "Resultado: " + suma;
+}
