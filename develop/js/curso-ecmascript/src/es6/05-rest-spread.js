c9d2b401b48edbf83726e1362903489cef2a4bed
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-17 17:53:20 -0500

    destructuring array and objects

---
+++ develop/js/curso-ecmascript/src/es6/05-rest-spread.js
@@ -0,0 +1,12 @@
+// array destructuring
+
+let fruits = ["apple", "banana"];
+
+let [a, b] = fruits;
+console.log(a, b);
+
+// object destructuring
+
+let user = { username: "Jhon", age: 32 };
+let { username, age } = user;
+console.log( username, age )
