40f677134136e0b4255834291a99a8d67653666c
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-17 17:11:41 -0500

    string

---
+++ develop/js/curso-ecmascript/src/es6/03-strings.js
@@ -0,0 +1,22 @@
+let hello = "hello";
+let world = "world";
+
+let saludo = hello + ' ' +  world +'!';
+
+console.log(saludo);
+
+// Template literals
+let phrase = `${hello} ${world}!`;
+console.log(phrase);
+
+// Multi-line string
+let loren = "esto es un string \n" + "esto es una linea";
+
+let loren2 = `esto es un string\nesto es una linea`;
+
+let loren3 = `esto es una linea
+esto es otra linea`;
+
+console.log(loren);
+console.log(loren2);
+console.log(loren3);
