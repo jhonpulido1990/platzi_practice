ce0d59378edb8bbbb708c618ea9b5564689b92b8
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-17 11:57:15 -0500

    add code let var const

--- develop/js/curso-ecmascript/src/es6/00-let-const.js
+++ develop/js/curso-ecmascript/src/es6/00-let-const.js
@@ -10,4 +10,15 @@ const animal = "dog";
 animal = "cat"; // no se puede realizar esta accion por que es una variable constante
 console.log(animal);
 
+const fruits = () => {
+  if (true) {
+    var fruit1 = "apple";
+    let fruit2 = "kiwi";
+    const fruit3 = "banana"
+  }
+  console.log(fruit1); // funtion scope
+  console.log(fruit2); // block scope
+  console.log(fruit3); // block scope
+}
 
+fruits();
