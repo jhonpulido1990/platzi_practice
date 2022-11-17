62a4ddc021972728da155c9c7aed5bfb93bf6a6a
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-17 11:51:19 -0500

    var let and const

---
+++ develop/js/curso-ecmascript/src/es6/00-let-const.js
@@ -0,0 +1,13 @@
+var lastname = "david";
+lastname = "oscar";
+console.log(lastname);
+
+let fruit = "apple";
+fruit = "kiwi";
+console.log(fruit);
+
+const animal = "dog";
+animal = "cat"; // no se puede realizar esta accion por que es una variable constante
+console.log(animal);
+
+
