8ff15bfeea5c556a2c6499272606aee2e5cd8560
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-18 15:24:34 -0500

    rest-spread

--- develop/js/curso-ecmascript/src/es6/05-rest-spread.js
+++ develop/js/curso-ecmascript/src/es6/05-rest-spread.js
@@ -9,4 +9,21 @@ console.log(a, b);
 
 let user = { username: "Jhon", age: 32 };
 let { username, age } = user;
-console.log( username, age )
+console.log(username, age)
+
+// spread operator
+
+let person = { name: "jhon", age: 32 };
+let country = "mx"
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 2, 3, 4, 5);
