9d829ec3713e000a747e5014ebf651b5504eb614
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-23 17:41:33 -0500

    actualizaciones es7

---
+++ develop/js/curso-ecmascript/src/es7/01-array-include.js
@@ -0,0 +1,8 @@
+let num = [1, 3, 4, 5, 6, 8, 9,];
+
+console.log(num.includes(4));
+
+const list = ['david', 'oscar', 'ana'];
+
+console.log(list.includes('oscar'));
+console.log(list.includes('Oscar'));
