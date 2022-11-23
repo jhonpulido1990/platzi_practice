8089546901b806f3c1e94c6ca30431d29ae703e8
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-23 17:20:17 -0500

    more information

--- develop/js/curso-ecmascript/src/es6/05-object-literals.js
+++ develop/js/curso-ecmascript/src/es6/05-object-literals.js
@@ -8,3 +8,40 @@ function newUser(user, age, country, uId) {
 }
 
 console.log(newUser("pablo", 32, "colombia", 1));
+
+/*
+Los objetos literales consiste en crear objetos a partir de variables sin repetir el nombre. Antes de ES6, para crear un objeto a partir de variables consistía en la siguiente manera:
+
+const nombre = "Andres"
+const edad = 23
+
+const objeto = {
+    nombre: nombre, 
+    edad: edad
+}
+
+objeto // { nombre: 'Andres', edad: 23 }
+Cómo utilizar objetos literales
+Con los parámetros de objeto puedes obviar la repetición de nombres, JavaScript creará la propiedad a partir del nombre de la variable con su respectivo valor.
+
+const nombre = "Andres"
+const edad = 23
+
+const objeto = {nombre, edad}
+
+objeto // { nombre: 'Andres', edad: 23 }
+El resultado es el mismo, pero sin la necesidad de repetir palabras. Puedes combinarlo con variables que su propiedad tiene un nombre diferente.
+
+const nombre = "Andres"
+const edad = 23
+const esteEsUnID = 1
+
+const objeto = {
+    nombre, 
+    edad,
+    id: esteEsUnID
+}
+
+objeto // { nombre: 'Andres', edad: 23, id: 1 }
+Contribución creada por Andrés Guano (Platzi Contributor).
+*/
