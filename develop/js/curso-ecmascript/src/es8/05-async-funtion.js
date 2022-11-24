7ab555c6c0c0ce5d9f2830c5de2ec9e5e4fc2b2c
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-24 15:39:41 -0500

    add information

--- develop/js/curso-ecmascript/src/es8/05-async-funtion.js
+++ develop/js/curso-ecmascript/src/es8/05-async-funtion.js
@@ -15,3 +15,33 @@ const anotherfuntion = async () => {
 console.log('Before');
 anotherfuntion();
 console.log('after');
+
+/*
+ES8: funciones asíncronas
+21/35
+
+RECURSOS
+MARCADORES
+En ECMAScript 2017 o ES8 fue añadida una nueva forma de manejar el asincronismo en JavaScript mediante funciones asíncronas.
+
+Cómo utilizar funciones asíncronas
+La función asíncrona se crea mediante la palabra reservada async y retorna una promesa.
+
+async function asyncFunction () {...}
+
+const asyncFunction = async () => { ... } 
+La palabra reservada await significa que espera hasta que una promesa sea resuelta y solo funciona dentro de una función asíncrona. Los bloques try / catch sirven para manejar si la promesa ha sido resuelta o rechazada.
+
+async function asyncFunction () {
+  try {
+    const response = await promesa()
+    return response
+  } catch (error) {
+    return error
+  }
+}
+¿Cuál es la mejor forma de manejar promesas, then o async / await? Ambas son muy útiles, manejar ambas te hará un mejor desarrollador.
+
+Curso de Asincronismo con JavaScript
+Contribución creada por Andrés Guano (Platzi Contributor).
+*/
