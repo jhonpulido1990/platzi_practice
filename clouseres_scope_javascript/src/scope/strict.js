caf7f3907b7ad4a637b4a062d377143a32b55cdb
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-19 14:22:28 -0500

    strict mode

---
+++ clouseres_scope_javascript/src/scope/strict.js
@@ -0,0 +1,11 @@
+'use strict'
+pi = 3.16 // genera error por falta de declararcion
+
+console.log(pi)
+
+function myfuntion() {
+    'use strict'
+    return pi = 3.1416; // genera error por falta de declararcion
+};
+
+console.log(myfuntion())
