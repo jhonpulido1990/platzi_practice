718d6c1cb50d2d0b84461ced72d53a4146ae7128
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-19 11:46:18 -0500

    reasignacion redclaracion

---
+++ clouseres_scope_javascript/src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // undefined
+firstName = 'jhon' // asignacion
+console.log(firstName);
+
+var lastName = 'pulido'; // declarar / asignacion
+lastName = 'julio' //re-asignacion
+console.log(lastName)
+
+// var
+
+var secondName = 'david' // declaracion / asignacion
+var secondName = 'pablo' // re-asignacion
+console.log(secondName)
+
+// let
+
+let fruit = 'apple' // declarar/ asignar
+fruit = 'kiwi' // re-asignar
+console.log(fruit)
+// let fruit = 'banana' //no se puede re-declarar nuevamente
+
+// const
+
+const animal = 'dog';
+// animal = 'cat' // no se puede re-asignar
+console.log(animal)
+
+const vehiculo = []
+vehiculo.push('hola')
+console.log(vehiculo)
+
+vehiculo.pop()
+console.log(vehiculo)
