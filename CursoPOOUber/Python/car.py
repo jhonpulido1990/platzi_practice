9a41b56b7da5170dbc9717eaa9b7c15d9fc8f5e1
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-03 10:38:52 -0500

    herencia en python

--- CursoPOOUber/Python/car.py
+++ CursoPOOUber/Python/car.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+from account import Account
 
 class Car:
     id = int
     licence = str
-    driver = str
-    passegenger = str
+    driver = Account("","")
+    passegenger = int
+
+    def __init__(self, licence, driver):
+        self.licence = licence
+        self.driver = driver
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 from car import Car
+from account import Account
 
 if __name__ == '__main__':
-    car = Car()
-    car.licence = "AMS132"
-    car.driver = "andres herrera"
+    car = Car("AMS132", Account("andres herrera","ORA132"))
     print(vars(car))
+    print(vars(car.driver))
