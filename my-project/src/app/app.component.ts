f97c97b58947f0b86a5fe94799951b894711e0a9
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-09 15:55:33 -0500

    new projects

---
+++ my-project/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  name = 'jhon';
+  age = 18;
+  img = 'https://avatars.githubusercontent.com/u/2975064?s=200&v=4'
+}
