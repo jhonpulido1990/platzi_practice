8f111bf7710fbf1ba50d52c81205415508a54280
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-15 16:47:47 -0500

    Omit y Pick Type

--- typescript-avanzado/src/app/main.ts
+++ typescript-avanzado/src/app/main.ts
@@ -1,27 +1,20 @@
 import { faker } from '@faker-js/faker';
-import { addProduct, products } from "./products/product.service";
+
+import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.datatype.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.imageUrl(),
     color: faker.commerce.color(),
-    size: faker.helpers.arrayElement(['M','S','L', 'XL']),
-    price: parseInt(faker.commerce.price()),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+    price: parseInt(faker.commerce.price(), 10),
     isNew: faker.datatype.boolean(),
-    tags: faker.helpers.arrayElement(),
+    tags: faker.helpers.arrayElements(),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
-    stock: faker.datatype.number({min:10, max: 100}),
-    category: {
-      id: faker.datatype.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    }
-  })
+    stock: faker.datatype.number({min: 10, max: 100}),
+    categoryId: faker.datatype.uuid()
+  });
 }
 
-console.log(products)
+console.log(products);
