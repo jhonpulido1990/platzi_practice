52ed2b15dc87a0083b2ce52da847e79233f6f7a4
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-16 16:56:55 -0500

    crud

--- typescript-avanzado/src/app/base.model.ts
+++ typescript-avanzado/src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface Basemodel {
-  readonly id: string | number;
+  readonly id: string;
   readonly createdAt: Date;
   updatedAt: Date;
 }
--- typescript-avanzado/src/app/main.ts
+++ typescript-avanzado/src/app/main.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-import { addProduct, products } from './products/product.service';
+import { addProduct, products, updateProduct, findProducts } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
@@ -18,3 +18,17 @@ for (let index = 0; index < 50; index++) {
 }
 
 console.log(products);
+const product = products[0];
+updateProduct(product.id, {
+  title: 'New title',
+  stock: 80,
+})
+
+
+findProducts({
+  stock: 10,
+  color: 'red',
+  createdAt: new Date(),
+  isNew: true,
+  tags: ['as', 'as']
+})
