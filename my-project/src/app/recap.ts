5c8dd4e748bfabe087dba7cbaed211176b463a6c
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-23 11:47:49 -0500

    concepto de typescript

---
+++ my-project/src/app/recap.ts
@@ -0,0 +1,12 @@
+const username: string = 'jhon';
+const sum = (a: number, b: number) => {
+  return a +b
+}
+
+sum(1, 3);
+
+class Person {
+  constructor(public age: number, public lastName: string) {}
+}
+
+const nico = new Person(15, 'molina');
