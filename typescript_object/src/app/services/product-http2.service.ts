f796fc6750ca324a1d21e19e6991bc821190db3d
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-23 10:55:40 -0500

    decoradores

---
+++ typescript_object/src/app/services/product-http2.service.ts
@@ -0,0 +1,10 @@
+import { Product } from "../model/product.model";
+import { BaseHttpService } from "./base-http.service";
+
+export class ProductHttpService extends BaseHttpService<Product> {
+  otroRequest() {
+    this.url;
+    //code
+    //code
+  }
+}
