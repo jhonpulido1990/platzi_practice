5b5129a4eb0d003f477e2394dbf38f9ab7b04dde
jhonpulido1990 <3825@holbertonstudents.com>
2023-03-24 17:32:17 -0500

    ngModel

--- my-project/src/app/app.component.ts
+++ my-project/src/app/app.component.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'jhon';
-  age = 18;
-  img = 'https://avatars.githubusercontent.com/u/2975064?s=200&v=4'
+  age = 30;
+  img = 'https://avatars.githubusercontent.com/u/2975064?s=200&v=4';
+  btnDisabled = true;
+  Person = {
+    name: 'jhon',
+    age: 30,
+    img: 'https://avatars.githubusercontent.com/u/2975064?s=200&v=4'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.Person.age++
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.Person.name = element.value
+  }
 }
