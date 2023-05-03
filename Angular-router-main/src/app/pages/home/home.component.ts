5acc968f970d6e10fee8323322f137227e2705b8
jhonpulido1990 <3825@holbertonstudents.com>
2023-05-03 15:59:55 -0500

    refactorizacion del home

--- Angular-router-main/src/app/pages/home/home.component.ts
+++ Angular-router-main/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
+
+  constructor(
+    private productsService: ProductsService
+  ){ }
+
+  ngOnInit(): void {
+    this.productsService.getAll(10, 0).subscribe((data) => {
+    this.products = data;
+    this.offset += this.limit;
+    });
+  }
+
+  onLoadMore() {
+    this.productsService.getAll(this.limit, this.offset).subscribe((data) => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
+    });
+  }
 }
