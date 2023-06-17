e2aa985fb92c25448e4872c46d2bf48c60475a12
jhonpulido1990 <3825@holbertonstudents.com>
2023-06-17 00:00:53 -0500

    shared module

--- Angular-router-main/src/app/website/website.module.ts
+++ Angular-router-main/src/app/website/website.module.ts
@@ -3,14 +3,9 @@ import { CommonModule } from '@angular/common';
 import { SwiperModule } from 'swiper/angular';
 
 import { WebsiteRoutingModule } from './website-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
@@ -23,13 +18,7 @@ import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
     CategoryComponent,
     MyCartComponent,
@@ -40,6 +29,6 @@ import { LayoutComponent } from './components/layout/layout.component';
     ProductDetailComponent,
     LayoutComponent,
   ],
-  imports: [CommonModule, WebsiteRoutingModule, SwiperModule],
+  imports: [CommonModule, WebsiteRoutingModule, SwiperModule, SharedModule],
 })
 export class WebsiteModule {}
