6cde93cc81f7c0f5816cb1e9d0b588a7b866d14f
jhonpulido1990 <3825@holbertonstudents.com>
2023-06-20 10:13:01 -0500

    state login

--- Angular-router-main/src/app/guards/auth.guard.ts
+++ Angular-router-main/src/app/guards/auth.guard.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TokenService } from '../services/token.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +14,28 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     private tokenService: TokenService,
+    private AuthService: AuthService,
     private router: Router,
   ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const token = this.tokenService.getToken()
+    /* const token = this.tokenService.getToken()
     if (!token) {
       this.router.navigate(['/home'])
     }
-    return true;
+    return true; */
+    return this.AuthService.user$
+    .pipe(
+      map(user => {
+        if (!user) {
+          this.router.navigate(['/home']);
+          return false;
+        }
+        return true;
+      })
+    )
   }
 
 }
