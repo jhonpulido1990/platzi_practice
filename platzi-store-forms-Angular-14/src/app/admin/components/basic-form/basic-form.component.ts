882fc0857416c65212b7c509c63e68e9526ff324
jhonpulido1990 <3825@holbertonstudents.com>
2023-06-30 10:50:14 -0500

    11 validators

--- platzi-store-forms-Angular-14/src/app/admin/components/basic-form/basic-form.component.ts
+++ platzi-store-forms-Angular-14/src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -7,21 +12,11 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    color: new FormControl('#000000'),
-    date: new FormControl(''),
-    age: new FormControl(12),
-    category: new FormControl('category-2'),
-    tag: new FormControl(''),
-    agree: new FormControl(false),
-    gender: new FormControl(''),
-    zone: new FormControl(''),
-  });
+  form: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
     this.nameField.valueChanges.subscribe((value) => {
@@ -30,7 +25,27 @@ export class BasicFormComponent implements OnInit {
   }
 
   save(event) {
-    console.log(this.form.value);
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z ]+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      color: ['#000000'],
+      date: [''],
+      age: [18, [Validators.required, Validators.min(18), Validators.max(100)]],
+      category: [''],
+      tag: [''],
+      agree: [false, [Validators.requiredTrue]],
+      gender: [''],
+      zone: [''],
+    });
   }
 
   getNameValue() {
