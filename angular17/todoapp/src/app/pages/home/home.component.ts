0aeca549692690a82c3443e68d50d7ae77fd064e
jhonpulido1990 <3825holbertonstudents.com>
2024-01-19 11:05:30 -0500

    mais atualizacoe

--- angular17/todoapp/src/app/pages/home/home.component.ts
+++ angular17/todoapp/src/app/pages/home/home.component.ts
@@ -1,39 +1,77 @@
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  task = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'crear proyecto',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'crear componentes',
-      completed: false
-    },
-  ]);
+export class HomeComponent implements OnInit {
+  task = signal<Task[]>([]);
+  filter = signal<'all' | 'pending' | 'completed'>('all');
 
-  changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.addTask(newTask);
+  taskByFilter = computed(() => {
+    const filter = this.filter();
+    const task = this.task();
+    if (filter === 'pending') {
+      return task.filter((task) => !task.completed);
+    }
+    if (filter === 'completed') {
+      return task.filter((task) => task.completed);
+    }
+    return task;
+  });
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
+  injector = inject(Injector);
+
+  ngOnInit(): void {
+    const storage = localStorage.getItem('tasks');
+    if (storage) {
+      const tasks = JSON.parse(storage);
+      this.task.set(tasks);
+    }
+    this.trackTask();
+  }
+
+  trackTask() {
+    effect(() => {
+      const tasks = this.task();
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, { injector: this.injector });
+  }
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
   }
 
   addTask(title: string) {
     const newTask = {
       id: Date.now(),
       title,
-      completed: false
-    }
+      completed: false,
+    };
     this.task.update((tasks) => [...tasks, newTask]);
   }
 
@@ -50,15 +88,54 @@ export class HomeComponent {
         if (position === index) {
           return {
             ...tas,
-            completed: !tas.completed
-          }
+            completed: !tas.completed,
+          };
         }
-        return tas
-      })
-    })
+        return tas;
+      });
+    });
+  }
+
+  updateTaskEditingMode(index: number) {
+    this.task.update((tasks) => {
+      return tasks.map((tas, position) => {
+        if (position === index) {
+          return {
+            ...tas,
+            editing: true,
+          };
+        }
+        return {
+          ...tas,
+          editing: false,
+        };
+      });
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.task.update((tasks) => {
+      return tasks.map((tas, position) => {
+        if (position === index) {
+          return {
+            ...tas,
+            title: input.value,
+            editing: false,
+          };
+        }
+        return tas;
+      });
+    });
   }
 
   deleteTask(index: number) {
-    this.task.update((tasks) => tasks.filter((tas, position)=> position !== index))
+    this.task.update((tasks) =>
+      tasks.filter((tas, position) => position !== index)
+    );
+  }
+
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
   }
 }
